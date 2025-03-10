524af2431bbff920c181b225b32b0e807d84a97c
sanju.shaji <sanjucrz007@gmail.com>
2025-03-10 17:48:32 +0530

    implemented the review

---
+++ src/main/java/org/example/LogAggregatorTool.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.File;
+
+import static org.example.Strings.*;
+
+public class LogAggregatorTool {
+    public static void main(String[] args) {
+
+        String userFilePath = args[0];
+        File userFolder = new File(userFilePath);
+        String[] folderContents = userFolder.list();
+        if (!(userFolder.isDirectory())) {
+            System.out.println(invalidPath);
+            return;
+        }
+        if (folderContents.length == 0) {
+            System.out.println(emptyFolder);
+            return;
+        }
+        for (String files : folderContents) {
+            System.out.println(files);
+            if (!(files.endsWith(".txt"))) {
+                System.out.println(extensionchecker);
+                return;
+            }
+        }
+        System.out.println(processing);
+    }
+}
---
+++ src/main/java/org/example/Strings.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Strings {
+
+
+    public static final String  invalidPath="Folder does not exists";
+
+    public static final String emptyFolder ="Folder is empty";
+    public static final  String processing ="Processing...";
+
+    public static final String extensionchecker="Files extension not supported";
+
+
+
+
+}
