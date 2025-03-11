8149d98334c3345ba73a44bdd4637791016458cd
sanju.shaji <sanjucrz007@gmail.com>
2025-03-11 14:05:35 +0530

    revised the code based on comments and resolved the comments

---
+++ src/main/java/tool/log_aggregator/LogAggregatorTool.java
@@ -0,0 +1,47 @@
+package tool.log_aggregator;
+
+import java.io.File;
+import java.util.Arrays;
+import static tool.log_aggregator.LogAggregatorToolConstants.INVALID_LOG_FILES;
+import static tool.log_aggregator.LogAggregatorToolConstants.EMPTY_FOLDER;
+import static tool.log_aggregator.LogAggregatorToolConstants.PROCESSING;
+import static tool.log_aggregator.LogAggregatorToolConstants.INVALID_PATH;
+import static tool.log_aggregator.LogAggregatorToolConstants.VALID_LOG_FILES;
+import static tool.log_aggregator.LogAggregatorToolConstants.TOTAL_FILE_COUNT;
+import static tool.log_aggregator.LogAggregatorToolConstants.SLASH;
+
+public class LogAggregatorTool {
+    public static void main(String[] args) {
+        String userFilePath = args[0];
+        File userFolder = new File(userFilePath);
+        String[] folderContents = userFolder.list();
+        if (!(userFolder.isDirectory())) {
+            System.out.println(INVALID_PATH);
+            return;
+        }
+        if (folderContents.length == 0) {
+            System.out.println(EMPTY_FOLDER);
+            return;
+        }
+        System.out.println(PROCESSING);
+        int logFileCount = 0;
+        int nonLogFilesCount = 0;
+        boolean noInvalidFlag=false;
+        long totalFilesCount = (Arrays.stream(folderContents).count());
+        for (String files : folderContents) {
+            if ((files.endsWith(".log"))) {
+                logFileCount += 1;
+            } else {
+                if (!(files.endsWith(".log"))) {
+                    nonLogFilesCount += 1;
+                    noInvalidFlag=true;
+
+                }
+            }
+        }
+        System.out.println(TOTAL_FILE_COUNT + totalFilesCount);
+        if(noInvalidFlag){
+        System.out.println(+nonLogFilesCount + SLASH + totalFilesCount + INVALID_LOG_FILES);}
+        System.out.println(logFileCount + SLASH + totalFilesCount + VALID_LOG_FILES);
+    }
+}
---
+++ src/main/java/tool/log_aggregator/LogAggregatorToolConstants.java
@@ -0,0 +1,20 @@
+package tool.log_aggregator;
+
+public class LogAggregatorToolConstants {
+    public static final String EMPTY_FOLDER ="Folder is empty";
+    public static final String INVALID_LOG_FILES=" are invalid log files";
+    public static final String  INVALID_PATH="Invalid Folder Path. Please Provide a valid folder path";
+    public static final  String PROCESSING ="Processing...";
+    public  static  final  String SLASH="\\";
+    public static final String TOTAL_FILE_COUNT="Total number file in the specified Folder : ";
+    public static final String VALID_LOG_FILES=" are valid files";
+
+
+
+
+
+
+
+
+
+}
