353490b5453492992bbadf4583ededb54f464d7a
sanju.shaji <sanjucrz007@gmail.com>
2025-03-11 15:16:29 +0530

    revised the code based on comments and resolved the comments

--- src/main/java/tool/log_aggregator/LogAggregatorToolConstants.java
+++ src/main/java/tool/log_aggregator/LogAggregatorToolConstants.java
@@ -2,7 +2,7 @@ package tool.log_aggregator;
 
 public class LogAggregatorToolConstants {
     public static final String EMPTY_FOLDER ="Folder is empty";
-    public static final String INVALID_LOG_FILES=" are invalid log files";
+    public static final String INVALID_LOG_FILES=" are invalid files";
     public static final String  INVALID_PATH="Invalid Folder Path. Please Provide a valid folder path";
     public static final  String PROCESSING ="Processing...";
     public  static  final  String SLASH="\\";
